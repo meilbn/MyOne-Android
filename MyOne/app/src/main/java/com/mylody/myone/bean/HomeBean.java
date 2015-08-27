a0c674dc2159e4903a72df153494a8cdce45b9de
ihappyhacking <codingallnight@gmail.com>
2015-08-27 16:49:25 +0800

    Add Glide

---
+++ MyOne/app/src/main/java/com/mylody/myone/bean/HomeBean.java
@@ -0,0 +1,10 @@
+package com.mylody.myone.bean;
+
+/**
+ * User: HappyHacking
+ * Date: 2015-08-27
+ * Time: 11:22
+ * Description:
+ */
+public class HomeBean {
+}
---
+++ MyOne/app/src/main/java/com/mylody/myone/module/HomeModel.java
@@ -0,0 +1,10 @@
+package com.mylody.myone.module;
+
+/**
+ * User: HappyHacking
+ * Date: 2015-08-27
+ * Time: 14:37
+ * Description:
+ */
+public class HomeModel {
+}
